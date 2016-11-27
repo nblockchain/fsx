8b9fce6fd7ced7d8bd935a0f447fafbb3fadceba
Andres G. Aragoneses <knocte@gmail.com>
2016-11-27 19:58:41 +0800

    hacky build (instead of calling fsharpc, add Env.Exit() and run)
    The Process class is implemented from my own S.O. answer:
    http://stackoverflow.com/a/40820711/544947

--- Infra.fs
+++ Infra.fs
@@ -2,6 +2,71 @@
 namespace Fsx.Infrastructure
 
 open System
+open System.Text
+open System.Threading
+open System.Diagnostics
+
+module Process =
+    let Execute (commandWithArguments: string, echo: bool, hidden: bool)
+        : int * string * string =
+
+        let outBuilder = new StringBuilder()
+        let errBuilder = new StringBuilder()
+
+        use outWaitHandle = new AutoResetEvent(false)
+        use errWaitHandle = new AutoResetEvent(false)
+
+        if (echo) then
+            Console.WriteLine(commandWithArguments)
+
+        let firstSpaceAt = commandWithArguments.IndexOf(" ")
+        let (command, args) =
+            if (firstSpaceAt >= 0) then
+                (commandWithArguments.Substring(0, firstSpaceAt), commandWithArguments.Substring(firstSpaceAt + 1))
+            else
+                (commandWithArguments, String.Empty)
+
+        let startInfo = new ProcessStartInfo(command, args)
+        startInfo.UseShellExecute <- false
+        startInfo.RedirectStandardOutput <- true
+        startInfo.RedirectStandardError <- true
+        use proc = new System.Diagnostics.Process()
+        proc.StartInfo <- startInfo
+
+        let outReceived (e: DataReceivedEventArgs): unit =
+            if (e.Data = null) then
+                outWaitHandle.Set() |> ignore
+            else
+                if not (hidden) then
+                    Console.WriteLine(e.Data)
+                outBuilder.AppendLine(e.Data) |> ignore
+
+        let errReceived (e: DataReceivedEventArgs): unit =
+            if (e.Data = null) then
+                errWaitHandle.Set() |> ignore
+            else
+                if not (hidden) then
+                    Console.Error.WriteLine(e.Data)
+                errBuilder.AppendLine(e.Data) |> ignore
+
+        proc.OutputDataReceived.Add outReceived
+        proc.ErrorDataReceived.Add errReceived
+
+        let exitCode =
+            try
+                proc.Start() |> ignore
+                proc.BeginOutputReadLine()
+                proc.BeginErrorReadLine()
+
+                proc.WaitForExit()
+                proc.ExitCode
+
+            finally
+                outWaitHandle.WaitOne() |> ignore
+                errWaitHandle.WaitOne() |> ignore
+
+        (exitCode, outBuilder.ToString(), errBuilder.ToString())
+
 
 module Util =
 
--- fsx.fsx
+++ fsx.fsx
@@ -2,10 +2,10 @@
 
 open System
 open System.IO
+open System.Linq
 
 #load "Infra.fs"
 open Fsx.Infrastructure
-
 
 let PrintUsage () =
     Console.WriteLine("Usage: ./fsx.fsx  [OPTION]... yourscript.fsx")
@@ -39,10 +39,57 @@ let rec FindScript(args: string list, script: Option<FileInfo>): Option<FileInfo
                 failwith (sprintf "Only one .fsx script allowed")
             FindScript(tail, script)
 
+let BuildFsxScript(script: FileInfo) : bool =
+    // TODO: this is good enough to catch syntax errors, but doesn't catch semantic errors;
+    // for that we would need to get the contents inside an [<EntryPoint>] func, and compile with fsharpc
+
+    if (script = null) then
+        raise(new ArgumentNullException("script"))
+
+    Console.WriteLine("Building {0}", script)
+    let bakFile = script.FullName.Replace(".fsx", ".fsx.bak")
+    File.Copy(script.FullName, bakFile, true)
+    let contents = File.ReadAllText(script.FullName)
+
+    let lines = contents.Split([| Environment.NewLine |], StringSplitOptions.None)
+
+    let newContents =
+        let exitCall = "System.Environment.Exit(0)"
+        if (lines.Any() && lines.[0].StartsWith("#")) then
+            let allLinesExceptFirst = Array.skip 1 lines
+            let scriptMinusShebang = String.Join(Environment.NewLine, allLinesExceptFirst)
+            String.Format("{0}{1}{2}{1}{3}", lines.[0], Environment.NewLine, exitCall, scriptMinusShebang)
+        else
+            String.Format("{0}{1}{2}", exitCall, Environment.NewLine, contents)
+
+    let exitCode =
+        try
+            File.WriteAllText(script.FullName, newContents)
+            let exitCode,_,_ = Process.Execute("fsharpi " + script.FullName, true, false)
+            exitCode
+
+        finally
+            File.Delete(script.FullName)
+            File.Move(bakFile, script.FullName)
+
+    let success =
+        match exitCode with
+        | 0 -> true
+        | _ -> false
+
+    if not (success) then
+        Console.Error.WriteLine("Build failure")
+    Console.WriteLine()
+
+    success
+
 let maybeScript = FindScript(args, None)
 match maybeScript with
 | None ->
     Console.Error.WriteLine("At least one .fsx script is required as input. Use --help for info.")
     Environment.Exit(1)
 | Some(script) ->
-    Console.WriteLine("Going to run " + script.FullName)
+    if (BuildFsxScript(script)) then
+        Environment.Exit(0)
+    else
+        Environment.Exit(1)
