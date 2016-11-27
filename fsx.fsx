421c0fd3952fbf25984c45beb9cb78c36125d671
Andres G. Aragoneses <knocte@gmail.com>
2016-11-27 19:07:40 +0800

    Print the usage (help) of command, if no args

--- fsx.fsx
+++ fsx.fsx
@@ -1,10 +1,22 @@
 #!/usr/bin/env fsharpi
 
+open System
+
 #load "Infra.fs"
 open Fsx.Infrastructure
 
-System.Console.WriteLine("hello world")
+
+let PrintUsage () =
+    Console.WriteLine("Usage: ./fsx.fsx  [OPTION]... yourscript.fsx")
+    Console.WriteLine()
+    Console.WriteLine("Options")
+    Console.WriteLine("  -c, --compile     Only compile, don't run (ideal for CI build scripts)")
 
 let args = Util.FsxArguments()
+if (args.Length = 0) then
+    PrintUsage()
+    Environment.Exit(1)
+
+System.Console.WriteLine("hello world")
 for arg in args do
     System.Console.WriteLine("arg: " + arg)
