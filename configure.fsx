bcb167138e12171918799a43acac260b31dde65e
Andres G. Aragoneses <knocte@gmail.com>
2017-04-15 18:24:58 +0800

    Basic build system
    This allows people to run
    `./configure.sh && make && sudo make install`
    to install fsx system-wide.
    
    The scripts have the absolute minimal in bash/shell
    syntax, and the majority of it in F# to mimic this
    basic 3 typical autotools commands.

---
+++ configure.fsx
@@ -0,0 +1,103 @@
+#!/usr/bin/env fsharpi
+
+open System
+open System.IO
+#load "Infra.fs"
+open FSX.Infrastructure
+
+Console.Write "checking for F# compiler... "
+let fsharpCompiler = "fsharpc"
+let fsharpcWhich = Process.Execute(sprintf "which %s" fsharpCompiler, false, true)
+if (fsharpcWhich.ExitCode <> 0) then
+    Console.Error.WriteLine("not found")
+    Console.Error.WriteLine(sprintf "configuration failed, please install \"%s\"" fsharpCompiler)
+    Environment.Exit(1)
+else
+    Console.WriteLine("found")
+
+let rec private GatherOrGetDefaultPrefix(args: string list, previousIsPrefixArg: bool, prefixSet: Option<string>): string =
+    let GatherPrefix(newPrefix: string): Option<string> =
+        match prefixSet with
+        | None -> Some(newPrefix)
+        | _ -> failwith ("prefix argument duplicated")
+
+    let prefixArgWithEquals = "--prefix="
+    match args with
+    | [] ->
+        match prefixSet with
+        | None -> "/usr/local"
+        | Some(prefix) -> prefix
+    | head::tail ->
+        if (previousIsPrefixArg) then
+            GatherOrGetDefaultPrefix(tail, false, GatherPrefix(head))
+        else if head = "--prefix" then
+            GatherOrGetDefaultPrefix(tail, true, prefixSet)
+        else if head.StartsWith(prefixArgWithEquals) then
+            GatherOrGetDefaultPrefix(tail, false, GatherPrefix(head.Substring(prefixArgWithEquals.Length)))
+        else
+            failwith (sprintf "argument not recognized: %s" head)
+
+let prefix = DirectoryInfo(GatherOrGetDefaultPrefix(Util.FsxArguments(), false, None))
+
+if not (prefix.Exists) then
+    let warning = sprintf "WARNING: prefix doesn't exist: %s" prefix.FullName
+    Console.Error.WriteLine (warning)
+
+File.WriteAllText(Path.Combine(__SOURCE_DIRECTORY__, "build.config"),
+                  sprintf "Prefix=%s" prefix.FullName)
+
+let versionContents = File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, "version.config"))
+let version = versionContents.Substring("Version=".Length).Trim()
+
+let GetRepoInfo()=
+    let rec GetBranchFromGitBranch(outchunks)=
+        match outchunks with
+        | [] -> failwith "current branch not found, unexpected output from `git branch`"
+        | head::tail ->
+            match head with
+            | StdErr(errChunk) ->
+                failwith ("unexpected stderr output from `git branch`: " + errChunk)
+            | StdOut(outChunk) ->
+                if (outChunk.StartsWith("*")) then
+                    let branchName = outChunk.Substring("* ".Length)
+                    branchName
+                else
+                    GetBranchFromGitBranch(tail)
+
+    let gitWhich = Process.Execute("which git", false, true)
+    if (gitWhich.ExitCode <> 0) then
+        String.Empty
+    else
+        let gitLog = Process.Execute("git log --oneline", false, true)
+        if (gitLog.ExitCode <> 0) then
+            String.Empty
+        else
+            let gitBranch = Process.Execute("git branch", false, true)
+            if (gitBranch.ExitCode <> 0) then
+                failwith "Unexpected git behaviour, as `git log` succeeded but `git branch` didn't"
+            else
+                let branch = GetBranchFromGitBranch(gitBranch.Output)
+                let gitLastCommit = Process.Execute("git log --no-color --first-parent -n1 --pretty=format:%h", false, true)
+                if (gitLastCommit.ExitCode <> 0) then
+                    failwith "Unexpected git behaviour, as `git log` succeeded before but not now"
+                else if (gitLastCommit.Output.Length <> 1) then
+                    failwith "Unexpected git output for special git log command"
+                else
+                    let lastCommitSingleOutput = gitLastCommit.Output.[0]
+                    match lastCommitSingleOutput with
+                    | StdErr(errChunk) ->
+                        failwith ("unexpected stderr output from `git log` command: " + errChunk)
+                    | StdOut(lastCommitHash) ->
+                        sprintf "(%s/%s)" branch lastCommitHash
+
+let repoInfo = GetRepoInfo()
+
+Console.WriteLine()
+Console.WriteLine(sprintf
+                      "\tConfiguration summary for fsx %s %s"
+                      version repoInfo)
+Console.WriteLine()
+Console.WriteLine(sprintf
+                      "\t* Installation prefix: %s"
+                      prefix.FullName)
+Console.WriteLine()
---
+++ make.fsx
@@ -0,0 +1,57 @@
+#!/usr/bin/env fsharpi
+
+open System
+open System.IO
+#load "Infra.fs"
+open FSX.Infrastructure
+
+let rec private GatherTarget(args: string list, targetSet: Option<string>): Option<string> =
+    match args with
+    | [] -> targetSet
+    | head::tail ->
+        if (targetSet.IsSome) then
+            failwith "only one target can be passed to make"
+        GatherTarget(tail, Some(head))
+
+let GatherPrefix(): string =
+    let buildConfig = FileInfo(Path.Combine(__SOURCE_DIRECTORY__, "build.config"))
+    if not (buildConfig.Exists) then
+        Console.Error.WriteLine ("ERROR: configure hasn't been run yet, run ./configure.sh first")
+        Environment.Exit 1
+    let buildConfigContents = File.ReadAllText(buildConfig.FullName)
+    buildConfigContents.Substring("Prefix=".Length).Trim()
+
+let prefix = GatherPrefix()
+let fsxInstallPath = Path.Combine(prefix, "lib", "fsx")
+let binInstallPath = Path.Combine(prefix, "bin")
+
+let fsxLauncherScriptPath = FileInfo(Path.Combine(__SOURCE_DIRECTORY__, "bin", "fsx"))
+let fsxBinaryPath = FileInfo(Path.Combine(__SOURCE_DIRECTORY__, "bin", "fsx.exe"))
+
+let JustBuild() =
+    Console.WriteLine("Compiling fsx...")
+    let fsxPath = Path.Combine(__SOURCE_DIRECTORY__, "fsx.fsx")
+    let fsharpcWhich = Process.Execute(sprintf "%s -c %s" fsxPath fsxPath, true, false)
+    if (fsharpcWhich.ExitCode <> 0) then
+        Environment.Exit 1
+    File.WriteAllText(fsxLauncherScriptPath.FullName,
+                      sprintf "#!/bin/sh\nexec mono %s/fsx.exe \"$@\"" fsxInstallPath)
+
+
+let maybeTarget = GatherTarget(Util.FsxArguments(), None)
+match maybeTarget with
+| None -> JustBuild()
+| Some(target) ->
+    if (target = "install") then
+        Console.WriteLine("Installing fsx...")
+        Console.WriteLine()
+        Directory.CreateDirectory(fsxInstallPath) |> ignore
+        File.Copy(fsxBinaryPath.FullName, Path.Combine(fsxInstallPath, fsxBinaryPath.Name), true)
+
+        let finalPrefixPathOfWrapperScript = Path.Combine(binInstallPath, fsxLauncherScriptPath.Name)
+        File.Copy(fsxLauncherScriptPath.FullName, finalPrefixPathOfWrapperScript, true)
+        if ((Process.Execute(sprintf "chmod ugo+x %s" finalPrefixPathOfWrapperScript, false, true)).ExitCode <> 0) then
+            failwith "Unexpected chmod failure, please report this bug"
+    else
+        Console.Error.WriteLine("Unrecognized target: " + target)
+        Environment.Exit 2
