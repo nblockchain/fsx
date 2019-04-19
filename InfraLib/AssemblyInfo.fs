0e814f294c12342ad41335b0a554047b8fad7318
Andres G. Aragoneses <knocte@gmail.com>
2019-04-20 02:22:02 +0800

    Converge to new first version 0.1.0.1

--- InfraLib/AssemblyInfo.fs
+++ InfraLib/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("InfraLib")>]
-[<assembly: AssemblyCopyright("Copyright © Andres G. Aragoneses & Gatecoin 2016-17")>]
+[<assembly: AssemblyCopyright("Copyright © 2016-2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.1.0.1")>]
+[<assembly: AssemblyFileVersion("0.1.0.1")>]
 
 do
     ()
