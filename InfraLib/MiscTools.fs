319b031bbeb34fc212d342b33af5b841f243ed77
Andres G. Aragoneses <knocte@gmail.com>
2019-04-20 15:12:48 +0800

    Add infrastructure to bump versions automatically

--- InfraLib/MiscTools.fs
+++ InfraLib/MiscTools.fs
@@ -390,5 +390,41 @@ module MiscTools =
         let gitlabUserEmail = Environment.GetEnvironmentVariable("GITLAB_USER_EMAIL")
         not (String.IsNullOrEmpty(gitlabUserEmail))
 
+    let GetCurrentVersion(dir: DirectoryInfo): Version =
+        let assemblyVersionFileName = "AssemblyInfo.fs"
+        let assemblyVersionFsFile =
+            (Directory.EnumerateFiles (dir.FullName,
+                                       assemblyVersionFileName,
+                                       SearchOption.AllDirectories)).SingleOrDefault ()
+        if assemblyVersionFsFile = null then
+            Console.Error.WriteLine (sprintf "%s not found in any subfolder (or found too many), cannot extract version number"
+                                             assemblyVersionFileName)
+            Environment.Exit 1
+
+        let assemblyVersionAttribute = "AssemblyVersion"
+        let lineContainingVersionNumber =
+            File.ReadLines(assemblyVersionFsFile).SingleOrDefault (fun line -> (not (line.Trim().StartsWith ("//"))) &&
+                                                                               line.Contains (assemblyVersionAttribute))
+
+        if lineContainingVersionNumber = null then
+            Console.Error.WriteLine (sprintf "%s attribute not found in %s (or found too many), cannot extract version number"
+                                             assemblyVersionAttribute assemblyVersionFsFile)
+            Environment.Exit 1
+
+        let versionNumberStartPosInLine = lineContainingVersionNumber.IndexOf("\"")
+        if versionNumberStartPosInLine = -1 then
+            Console.Error.WriteLine "Format unexpected in version string (expecting a starting double quote), cannot extract version number"
+            Environment.Exit 1
+
+        let versionNumberEndPosInLine = lineContainingVersionNumber.IndexOf("\"", versionNumberStartPosInLine + 1)
+        if versionNumberEndPosInLine = -1 then
+            Console.Error.WriteLine "Format unexpected in version string (expecting an ending double quote), cannot extract version number"
+            Environment.Exit 1
+
+        let version = lineContainingVersionNumber.Substring(versionNumberStartPosInLine + 1,
+                                                    versionNumberEndPosInLine - versionNumberStartPosInLine - 1)
+        Version(version)
+
+
     let SqlReadOnlyGroupName = "sqlreadonly"
 
