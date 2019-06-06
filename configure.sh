b3a6f65a8e80f954360fd2f21deb534440cb5506
Andres G. Aragoneses <knocte@gmail.com>
2019-06-06 20:11:38 +0800

    Migrate configure script out of fsharpi dependence
    Because fsharpi is broken in Ubuntu 19.04 (as we already
    noticed here[1]).
    
    Next step is the make.fsx migration to normal Makefile,
    that's why fsicheck.sh has not been removed yet.
    
    [1] ab59b7b8bacb6e837ab337fd0d421b617b94bba6

--- configure.sh
+++ configure.sh
@@ -2,4 +2,48 @@
 set -e
 
 ./fsicheck.sh configure
-./configure.fsx "$@"
+
+if ! which fsharpc >/dev/null 2>&1; then
+    echo "checking for F# compiler... not found"
+    exit 1
+else
+    echo "checking for F# compiler... found"
+fi
+
+if ! which xbuild >/dev/null 2>&1; then
+    echo "checking for xbuild... not found"
+    exit 1
+else
+    echo "checking for xbuild... found"
+fi
+
+DESCRIPTION="tarball"
+if which git >/dev/null 2>&1; then
+    # https://stackoverflow.com/a/12142066/1623521
+    DESCRIPTION=`git rev-parse --abbrev-ref HEAD`
+fi
+
+#default:
+PREFIX=/usr/local
+
+for i in "$@"
+do
+case $i in
+    -p=*|--prefix=*)
+    PREFIX="${i#*=}"
+
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+source version.config
+echo "Prefix=$PREFIX" > build.config
+
+echo
+echo -e "\tConfiguration summary for fsx $Version ($DESCRIPTION)"
+echo
+echo -e "\t* Installation prefix: $PREFIX"
+echo
