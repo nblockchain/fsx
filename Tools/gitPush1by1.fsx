71e857dad4b4dde92e4b9d91439eb58b4187c3f8
Andres G. Aragoneses <knocte@gmail.com>
2020-08-11 17:48:05 +0800

    Tools/gitPush1by1: use --force-with-lease instead of --force
    It's safer, to avoid overwriting other people's work.

--- Tools/gitPush1by1.fsx
+++ Tools/gitPush1by1.fsx
@@ -15,7 +15,8 @@ let GitSpecificPush (remoteName: string) (commitSha: string) (remoteBranchName: 
     let gitPush =
         {
             Command = "git"
-            Arguments = sprintf "push %s %s:%s -f" remoteName commitSha remoteBranchName
+            Arguments = sprintf "push %s %s:%s --force-with-lease"
+                                remoteName commitSha remoteBranchName
         }
     Process.SafeExecute (gitPush, Echo.OutputOnly) |> ignore
 
