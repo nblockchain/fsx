ed6aa46c12c77052d2cc7df6bc3722682a79fd74
Andres G. Aragoneses <knocte@gmail.com>
2020-08-16 15:43:39 +0800

    only 2fs files left

--- test/netStandardSample/src/GWallet.Backend/FiatValueEstimation.fs
+++ test/netStandardSample/src/GWallet.Backend/FiatValueEstimation.fs
@@ -5,7 +5,13 @@ open System.Net
 
 open FSharp.Data
 
-open GWallet.Backend.FSharpUtil.UwpHacks
+type Currency =
+    | BTC
+    | LTC
+    | ETH
+    | ETC
+    | DAI
+    | SAI
 
 module FiatValueEstimation =
     let private PERIOD_TO_CONSIDER_PRICE_STILL_FRESH = TimeSpan.FromMinutes 2.0
@@ -41,19 +47,19 @@ module FiatValueEstimation =
             let baseUrl =
                 match provider with
                 | PriceProvider.CoinCap ->
-                    SPrintF1 "https://api.coincap.io/v2/rates/%s" tickerName
+                    sprintf "https://api.coincap.io/v2/rates/%s" tickerName
                 | PriceProvider.CoinGecko ->
-                    SPrintF1 "https://api.coingecko.com/api/v3/simple/price?ids=%s&vs_currencies=usd" tickerName
+                    sprintf "https://api.coingecko.com/api/v3/simple/price?ids=%s&vs_currencies=usd" tickerName
             let uri = Uri baseUrl
             let task = webClient.DownloadStringTaskAsync uri
             let! res = Async.AwaitTask task
             return Some (tickerName,res)
         with
         | ex ->
-            if (FSharpUtil.FindException<WebException> ex).IsSome then
+            if ex.GetType() = typeof<WebException> then
                 return None
             else
-                return raise <| FSharpUtil.ReRaise ex
+                return raise ex
     }
 
     let private QueryCoinCap currency = async {
@@ -70,7 +76,7 @@ module FiatValueEstimation =
                     // interestingly this can throw in CoinCap because retreiving ethereum-classic doesn't work...
                     return None
                 else
-                    return raise <| FSharpUtil.ReRaise ex
+                    return raise ex
     }
 
     let private QueryCoinGecko currency = async {
@@ -82,10 +88,10 @@ module FiatValueEstimation =
             let parsedJsonObj = FSharp.Data.JsonValue.Parse json
             let usdPrice =
                 match parsedJsonObj.TryGetProperty ticker with
-                | None -> failwith <| SPrintF1 "Could not pre-parse %s" json
+                | None -> failwith <| sprintf "Could not pre-parse %s" json
                 | Some innerObj ->
                     match innerObj.TryGetProperty "usd" with
-                    | None -> failwith <| SPrintF1 "Could not parse %s" json
+                    | None -> failwith <| sprintf "Could not parse %s" json
                     | Some value -> value.AsDecimal()
             return Some usdPrice
     }
@@ -93,10 +99,8 @@ module FiatValueEstimation =
     let private RetrieveOnline currency = async {
         let coinGeckoJob = QueryCoinGecko currency
         let coinCapJob = QueryCoinCap currency
-        let bothJobs = FSharpUtil.AsyncExtensions.MixedParallel2 coinGeckoJob coinCapJob
-        let! maybeUsdPriceFromCoinGecko, maybeUsdPriceFromCoinCap = bothJobs
-        if maybeUsdPriceFromCoinCap.IsSome && currency = Currency.ETC then
-            Infrastructure.ReportWarningMessage "Currency ETC can now be queried from CoinCap provider?"
+        let! bothJobs = Async.Parallel [ coinGeckoJob ; coinCapJob ]
+        let maybeUsdPriceFromCoinGecko, maybeUsdPriceFromCoinCap = bothJobs.[0], bothJobs.[1]
         let result =
             match maybeUsdPriceFromCoinGecko, maybeUsdPriceFromCoinCap with
             | None, None -> None
@@ -117,29 +121,8 @@ module FiatValueEstimation =
                         ethMultiplied
                     else
                         price
-                Caching.Instance.StoreLastFiatUsdPrice(currency, realPrice)
                 realPrice |> Some
             | None -> None
         return realResult
-    }
-
-    let UsdValue(currency: Currency): Async<MaybeCached<decimal>> = async {
-        let maybeUsdPrice = Caching.Instance.RetrieveLastKnownUsdPrice currency
-        match maybeUsdPrice with
-        | NotAvailable ->
-            let! maybeOnlineUsdPrice = RetrieveOnline currency
-            match maybeOnlineUsdPrice with
-            | None -> return NotFresh NotAvailable
-            | Some value -> return Fresh value
-        | Cached(someValue,someDate) ->
-            if (someDate + PERIOD_TO_CONSIDER_PRICE_STILL_FRESH) > DateTime.UtcNow then
-                return Fresh someValue
-            else
-                let! maybeOnlineUsdPrice = RetrieveOnline currency
-                match maybeOnlineUsdPrice with
-                | None ->
-                    return NotFresh (Cached(someValue,someDate))
-                | Some freshValue ->
-                    return Fresh freshValue
     }
 
