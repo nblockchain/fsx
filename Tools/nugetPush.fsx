c21f6f1156d9d6350585b2c5fb1e985db76cc16d
Afshin Arani <afshinarani79@gmail.com>
2021-02-11 14:17:53 +0330

    nugetPush: fallback if dotnet sdk doesn't exist
    In case of repos that don't have .NET sdk installed,
    we should try and use "nuget.exe" for push as well.

--- Tools/nugetPush.fsx
+++ Tools/nugetPush.fsx
@@ -23,6 +23,14 @@ if args.Length > 3 then
 if args.Length > 2 && args.[0] <> "--output-version" then
     PrintUsage ()
 
+let rootDir =
+    DirectoryInfo (
+        Path.Combine (
+            __SOURCE_DIRECTORY__, // Tools/
+            "..",                 // fsx root
+            ".."                  // repo root
+        )                 
+    )
 
 // this is a translation of doing this in unix (assuming initialVersion="0.1.0"):
 // 0.1.0--date`date +%Y%m%d-%H%M`.git-`git rev-parse --short=7 HEAD`
@@ -41,11 +49,32 @@ let GetIdealNugetVersion (initialVersion: string) =
                                initialVersion dateSegment gitSegment
     finalVersion
 
+let IsDotNetSdkInstalled() = 
+    try
+        let dotnetVersionCmd =
+            {
+                Command = "dotnet"
+                Arguments = "--version"
+            }
+        Process.SafeExecute (dotnetVersionCmd, Echo.All) |> ignore 
+        true
+    with
+        :? ProcessCouldNotStart -> false
+
+let EnsureNugetExists() =
+    let nugetTargetDir = Path.Combine(rootDir.FullName, ".nuget") |> DirectoryInfo
+    if not nugetTargetDir.Exists then
+        nugetTargetDir.Create()
+    let prevCurrentDir = Directory.GetCurrentDirectory()
+    Directory.SetCurrentDirectory nugetTargetDir.FullName
+    let nugetDownloadUri = Uri "https://dist.nuget.org/win-x86-commandline/v4.5.1/nuget.exe"
+    Network.DownloadFile nugetDownloadUri |> ignore
+    let nugetExe = Path.Combine(nugetTargetDir.FullName, "nuget.exe") |> FileInfo
+    Directory.SetCurrentDirectory prevCurrentDir
+
+    nugetExe
+
 let FindOrGenerateNugetPackages (): seq<FileInfo> =
-    let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, // Tools/
-                                             "..",                 // fsx root
-                                             "..")                 // repo root
-                               )
     let nuspecFiles = rootDir.EnumerateFiles "*.nuspec"
     if nuspecFiles.Any() then
         if args.Length < 1 then
@@ -53,26 +82,18 @@ let FindOrGenerateNugetPackages (): seq<FileInfo> =
             Environment.Exit 1
         let baseVersion = args.First()
 
-        // we need to download nuget.exe because `dotnet pack` doesn't support using standalone (i.e.
-        // without a project association) .nuspec files, see https://github.com/NuGet/Home/issues/4254
-        let nugetTargetDir = Path.Combine(rootDir.FullName, ".nuget") |> DirectoryInfo
-        if not nugetTargetDir.Exists then
-            nugetTargetDir.Create()
-        let prevCurrentDir = Directory.GetCurrentDirectory()
-        Directory.SetCurrentDirectory nugetTargetDir.FullName
-        let nugetDownloadUri = Uri "https://dist.nuget.org/win-x86-commandline/v4.5.1/nuget.exe"
-        Network.DownloadFile nugetDownloadUri |> ignore
-        let nugetExe = Path.Combine(nugetTargetDir.FullName, "nuget.exe") |> FileInfo
-        Directory.SetCurrentDirectory prevCurrentDir
-
         seq {
             for nuspecFile in nuspecFiles do
                 let packageName = Path.GetFileNameWithoutExtension nuspecFile.FullName
 
                 let nugetVersion = GetIdealNugetVersion baseVersion
+                
+                // we need to download nuget.exe here because `dotnet pack` doesn't support using standalone (i.e.
+                // without a project association) .nuspec files, see https://github.com/NuGet/Home/issues/4254
+                
                 let nugetPackCmd =
                     {
-                        Command = nugetExe.FullName
+                        Command = EnsureNugetExists().FullName
                         Arguments = sprintf "pack %s -Version %s"
                                             nuspecFile.FullName nugetVersion
                     }
@@ -92,13 +113,16 @@ let FindOrGenerateNugetPackages (): seq<FileInfo> =
             let baseVersion = args.First()
             let nugetVersion = GetIdealNugetVersion baseVersion
 
-            let dotnetPackCmd =
-                {
-                    Command = "dotnet"
-                    Arguments = sprintf "pack -c Release -p:Version=%s"
-                                        nugetVersion
-                }
-            Process.SafeExecute (dotnetPackCmd, Echo.All) |> ignore
+            if IsDotNetSdkInstalled() then
+                let dotnetPackCmd =
+                    {
+                        Command = "dotnet"
+                        Arguments = sprintf "pack -c Release -p:Version=%s"
+                                            nugetVersion
+                    }
+                Process.SafeExecute (dotnetPackCmd, Echo.All) |> ignore
+            else 
+                failwith "Please install .NET SDK to build nuget packages without nuspec file"
 
         FindNugetPackages()
 
@@ -106,13 +130,22 @@ let FindOrGenerateNugetPackages (): seq<FileInfo> =
 let NugetUpload (packageFile: FileInfo) (nugetApiKey: string) =
 
     let defaultNugetFeedUrl = "https://api.nuget.org/v3/index.json"
-    let nugetPushCmd =
-        {
-            Command = "dotnet"
-            Arguments = sprintf "nuget push %s -k %s -s %s"
-                                packageFile.FullName nugetApiKey defaultNugetFeedUrl
-        }
-    Process.SafeExecute (nugetPushCmd, Echo.All) |> ignore
+    if IsDotNetSdkInstalled() then
+        let nugetPushCmd =
+            {
+                Command = "dotnet"
+                Arguments = sprintf "nuget push %s -k %s -s %s"
+                                    packageFile.FullName nugetApiKey defaultNugetFeedUrl
+            }
+        Process.SafeExecute (nugetPushCmd, Echo.All) |> ignore
+    else 
+        let nugetPushCmd =
+            {
+                Command = EnsureNugetExists().FullName
+                Arguments = sprintf "push %s -ApiKey %s -Source %s"
+                                    packageFile.FullName nugetApiKey defaultNugetFeedUrl
+            }
+        Process.SafeExecute (nugetPushCmd, Echo.All) |> ignore
 
 if args.Length > 0 && args.[0] = "--output-version" then
     if args.Length < 2 then
