b49c2ccd8a90b2c807931e2c559b610f11e28e1f
Andres G. Aragoneses <knocte@gmail.com>
2021-10-21 00:44:41 +0800

    Fix launcher.sh to allow passing flags (e.g. -v) to fsx again
    This got likely broken in a533f3be65b2528ca33b68ad07d9b07136cee79e.

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -11,8 +11,36 @@ fi
 
 DIR_OF_THIS_SCRIPT=$(dirname "$(realpath "$0")")
 FSXC_PATH="$DIR_OF_THIS_SCRIPT/../lib/fsx/fsxc.exe"
-mono "$FSXC_PATH" "$1"
-TARGET_DIR=$(dirname -- "$1")
-TARGET_FILE=$(basename -- "$1")
-shift
-exec mono "$TARGET_DIR/bin/$TARGET_FILE.exe" "$@"
+
+FIRST_ARGS=""
+FSX_SCRIPT=""
+REST_ARGS=""
+while [ $# -gt 0 ];
+do
+    ARG=$1
+    # FIXME: edge case: arg is simply "fsx", then below would think it had the
+    #        .fsx extension
+    EXTENSION="${ARG##*.}"
+
+    if [ -z "$FSX_SCRIPT" ]; then
+        FIRST_ARGS="$FIRST_ARGS $ARG"
+    fi
+    shift
+    if [ $EXTENSION = fsx ]; then
+        FSX_SCRIPT=$ARG
+        REST_ARGS=$@
+    fi
+done
+
+mono $FSXC_PATH $FIRST_ARGS
+
+if [ -z "$FSX_SCRIPT" ]; then
+    echo "Compilation of anything that is not an .fsx should have been rejected by fsx"
+    echo "and shouldn't have reached this point. Please report this bug."
+    exit 3
+fi
+
+TARGET_DIR=$(dirname -- "$FSX_SCRIPT")
+TARGET_FILE=$(basename -- "$FSX_SCRIPT")
+
+exec mono "$TARGET_DIR/bin/$TARGET_FILE.exe" $REST_ARGS
