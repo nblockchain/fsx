47a9c835666b3ae66d450dbaecebdaad103e3930
Andres G. Aragoneses <knocte@gmail.com>
2021-11-05 17:34:22 -0600

    scripts,test: check support for lib load out of tree

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,12 @@ set -euxo pipefail
 ./test/test.fsx
 
 # this generates a test.dll file
-cd ./test/ && fsharpc test.fs --target:library && cd ..
-
+cd ./test/ && fsharpc test.fs --target:library --out:test1.dll && cd ..
 ./test/testRefLib.fsx
+
+mkdir ./test/lib
+cd ./test/ && fsharpc test.fs --target:library --out:lib/test2.dll && cd ..
+./test/testRefLibOutsideCurrentFolder.fsx
+
 ./test/testTsv.fsx
 ./test/testProcessConcurrency.fsx
