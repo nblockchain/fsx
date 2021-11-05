24f79e29f4004779e35e36cdd9ed5d2ecb588b2a
Andres G. Aragoneses <knocte@gmail.com>
2021-11-05 11:39:49 -0600

    Test #r feature

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,10 @@
 set -euxo pipefail
 
 ./test/test.fsx
+
+# this generates a test.dll file
+cd ./test/ && fsharpc test.fs --target:library && cd ..
+
+./test/testRefLib.fsx
 ./test/testTsv.fsx
 ./test/testProcessConcurrency.fsx
---
+++ test/testRefLib.fsx
@@ -0,0 +1,6 @@
+#!/usr/bin/env fsx
+
+#r "test.dll"
+NS.Inside.Foo()
+
+System.Console.WriteLine("hello world")
