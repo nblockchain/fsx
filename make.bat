1eb23d00b9031b2ff131fa7bbf9a6c4dd7965dbf
Andres G. Aragoneses <knocte@gmail.com>
2022-07-26 13:00:14 +0800

    fixup

--- make.bat
+++ make.bat
@@ -1,20 +1,22 @@
+@ECHO OFF
 FOR /f "tokens=* delims=" %%A in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find MSBuild\**\Bin\MSBuild.exe') do set BUILDTOOL=%%A
 
 IF %1.==install. GOTO Install
 IF %1.==. GOTO JustBuild
-GOTO Error
+GOTO ErrorArg
 
 :Install
-    "%BUILDTOOL%" fsx.sln /p:Configuration=Release
-    mkdir "%ProgramFiles%\fsx"
-    copy fsxc\bin\Release\*.* "%ProgramFiles%\fsx"
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Release || EXIT /b
+    mkdir "%ProgramFiles%\fsx" || EXIT /b
+    copy fsxc\bin\Release\*.* "%ProgramFiles%\fsx" || EXIT /b
     GOTO End
 
 :JustBuild
-    "%BUILDTOOL%" fsx.sln /p:Configuration=Debug
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Debug || EXIT /b
     GOTO End
 
-:Error
+:ErrorArg
     ECHO "Argument not recognized: %1"
+    GOTO End
 
 :End
