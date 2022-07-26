a7b10ccf4b96bca4e9ee9d2e3d3959cde1b1a287
Andres G. Aragoneses <knocte@gmail.com>
2022-07-26 11:45:10 +0800

    GitHubCI,make.bat: implement basic 'install' in Win

--- make.bat
+++ make.bat
@@ -1,3 +1,22 @@
+@ECHO OFF
 FOR /f "tokens=* delims=" %%A in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find MSBuild\**\Bin\MSBuild.exe') do set BUILDTOOL=%%A
 
-"%BUILDTOOL%" fsx.sln
+IF %1.==install. GOTO Install
+IF %1.==. GOTO JustBuild
+GOTO ErrorArg
+
+:Install
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Release || EXIT /b
+    mkdir "%ProgramFiles%\fsx" || EXIT /b
+    copy fsxc\bin\Release\*.* "%ProgramFiles%\fsx" || EXIT /b
+    GOTO End
+
+:JustBuild
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Debug || EXIT /b
+    GOTO End
+
+:ErrorArg
+    ECHO "Argument not recognized: %1"
+    GOTO End
+
+:End
