b0585f36f64844f28fcfd1207143ef6221a7220b
Andres G. Aragoneses <knocte@gmail.com>
2022-07-26 11:45:10 +0800

    WIP1: add install target in windows

--- make.bat
+++ make.bat
@@ -1,3 +1,20 @@
 FOR /f "tokens=* delims=" %%A in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find MSBuild\**\Bin\MSBuild.exe') do set BUILDTOOL=%%A
 
-"%BUILDTOOL%" fsx.sln
+IF %1.==install. GOTO Install
+IF %1.==. GOTO JustBuild
+GOTO Error
+
+:Install
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Release
+    mkdir "%ProgramFiles%\fsx"
+    copy fsxc\bin\Release\*.* "%ProgramFiles%\fsx"
+    GOTO End
+
+:JustBuild
+    "%BUILDTOOL%" fsx.sln /p:Configuration=Debug
+    GOTO End
+
+:Error
+    ECHO "Argument not recognized: %1"
+
+:End
