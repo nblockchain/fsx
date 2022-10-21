3168f5a75e139d1b6c829f4d98d6c5400dc13ec7
Andres G. Aragoneses <knocte@gmail.com>
2022-10-21 15:05:32 +0800

    test: simpler test for Process API & output
    Previous commit disabled the testProcessConcurrency.fsx test but
    there's still value in it without the need to test the crossing
    of stdOut and stdErr streams, so we kind of mimic it in this new
    simpler test.

---
+++ test/testProcess.fsx
@@ -0,0 +1,47 @@
+#!/usr/bin/env fsx
+open System
+open System.IO
+
+#r "System.Configuration"
+open System.Configuration
+
+#load "../InfraLib/Misc.fs"
+#load "../InfraLib/Process.fs"
+
+open FSX.Infrastructure
+open Process
+
+let mutable retryCount = 0
+
+while (retryCount < 20) do //this is a stress test
+    let procResult =
+        Process.Execute(
+            {
+                Command = "fsharpi"
+                Arguments = "test/testProcessSample.fsx"
+            },
+            Echo.Off
+        )
+
+    let actual =
+        (procResult
+            .Output
+            .ToString()
+            .Replace(Environment.NewLine, "-"))
+
+    let expected = "foo-bar-baz-"
+
+    if (actual <> expected) then
+        Console.Error.WriteLine(
+            sprintf
+                "Stress test failed, got `%s`, should have been `%s`"
+                actual
+                expected
+        )
+
+        Environment.Exit 1
+
+    retryCount <- retryCount + 1
+
+Console.WriteLine "Success"
+Environment.Exit 0
