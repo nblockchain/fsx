5e15ef0e60371b1f500d79bd8bea8c4a59bf5894
Andres G. Aragoneses <knocte@gmail.com>
2022-10-31 13:58:29 +0800

    fixup

--- scripts/fsx.bat
+++ scripts/fsx.bat
@@ -2,11 +2,11 @@
 SET "FSIBAT=%ProgramFiles%\fsx\fsi.bat"
 SET "FSXFSX=%ProgramFiles%\fsx\fsx.fsx"
 
-IF NOT EXIST %FSIBAT% (
+IF NOT EXIST "%FSIBAT%" (
     ECHO "%FSIBAT% not found" && EXIT /b 1
 )
 
-IF NOT EXIST %FSXFSX% (
+IF NOT EXIST "%FSXFSX%" (
     ECHO "%FSXFSX% not found" && EXIT /b 1
 )
 
