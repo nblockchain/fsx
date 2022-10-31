80fc6de474bdf4f6e8f28989ab22d399b0eac6d2
Andres G. Aragoneses <knocte@gmail.com>
2022-10-31 14:07:40 +0800

    safeguards

--- scripts/fsx.bat
+++ scripts/fsx.bat
@@ -1,2 +1,12 @@
 @ECHO OFF
+SET "FSIBAT=%ProgramFiles%\fsx\fsi.bat"
+SET "FSXFSX=%ProgramFiles%\fsx\fsx.fsx"
+
+IF NOT EXIST "%FSIBAT%" (
+    ECHO "%FSIBAT% not found" && EXIT /b 1
+)
+
+IF NOT EXIST "%FSXFSX%" (
+    ECHO "%FSXFSX% not found" && EXIT /b 1
+)
 CALL "%ProgramFiles%\fsx\fsi.bat" "%ProgramFiles%\fsx\fsx.fsx" %* || EXIT /b
