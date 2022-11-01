310815fffe10b123218239fdc371371a2d9b858b
Andres G. Aragoneses <knocte@gmail.com>
2022-11-01 13:36:35 +0800

    test: improve fsiCommandLineArgs test

--- test/testFsiCommandLineArgs.fsx
+++ test/testFsiCommandLineArgs.fsx
@@ -11,23 +11,35 @@ if args.Length < 4 then
 
     Environment.Exit 1
 
+let expected = "?,one,2,three"
+let got = String.Join(",", args)
+
 if args.[1] <> "one" then
     Console.Error.WriteLine(
-        sprintf "Failed: expected 1st arg to be 'one', got '%s'" args.[1]
+        sprintf
+            "Failed: different 1st arg; expected '%s', got '%s'"
+            expected
+            got
     )
 
     Environment.Exit 2
 
 if args.[2] <> "2" then
     Console.Error.WriteLine(
-        sprintf "Failed: expected 2st arg to be '2', got '%s'" args.[2]
+        sprintf
+            "Failed: different 2nd arg; expected '%s', got '%s'"
+            expected
+            got
     )
 
     Environment.Exit 3
 
 if args.[3] <> "three" then
     Console.Error.WriteLine(
-        sprintf "Failed: expected 3rd arg to be 'three', got '%s'" args.[3]
+        sprintf
+            "Failed: different 3rd arg; expected '%s', got '%s'"
+            expected
+            got
     )
 
     Environment.Exit 4
