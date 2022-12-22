b84df2a40c6897dbc268029887e06214b5dee5b5
realmarv <zahratehraninasab@gmail.com>
2022-12-22 18:16:31 +0330

    incrementVersion: cleaning

--- scripts/CI/incrementVersion.fsx
+++ scripts/CI/incrementVersion.fsx
@@ -1,13 +1,9 @@
 open System
 open System.IO
 
-let version = 
-        File.ReadLines "version.txt" 
-        |> Seq.head
+let version = File.ReadLines "version.txt" |> Seq.head
 
-let splitted_version: array<int> = 
-        version.Split [|'.'|] 
-        |> Array.map System.Int32.Parse 
+let splitted_version: array<int> = version.Split [|'.'|] |> Array.map System.Int32.Parse 
 
 let major = splitted_version.[0]
 let minor = splitted_version.[1]
@@ -16,4 +12,3 @@ let patch = splitted_version.[2]
 let new_version: string = $"{major}.{minor}.{patch+1}"
 printfn "New version: %A" new_version
 File.WriteAllText("version.txt", new_version)
-
