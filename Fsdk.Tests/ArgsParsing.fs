208f23ec3454dd21d4a577ad4254f31313dddcaa
Andres G. Aragoneses <knocte@gmail.com>
2023-08-23 13:44:32 +0800

    Fsdk.Tests(ArgsParsing): augment (failing test)

--- Fsdk.Tests/ArgsParsing.fs
+++ Fsdk.Tests/ArgsParsing.fs
@@ -15,4 +15,6 @@ type ArgsParsing() =
         match res with
         | Misc.ArgsParsed.OnlyFlags flags ->
             Assert.That(Seq.length flags, Is.EqualTo 2)
+            Assert.That(Seq.item 0 flags, Is.EqualTo "--someLongFlag1")
+            Assert.That(Seq.item 1 flags, Is.EqualTo "-f2")
         | _ -> Assert.Fail "res was not ArgsParsing.OnlyFlags subtype"
